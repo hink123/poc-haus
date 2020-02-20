2cec17953906edac05ac5a0525b0741cb12d4861
Daniel Shamany <dshamany@gmail.com>
2020-02-20 13:36:57 -0800

    finish login functionality

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const logger = require('morgan');
 require('dotenv').config()
 require('./config/database');
 
+const userRouter = require('./routes/users');
+
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/users', userRouter);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,29 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignIn.css';
 import { Link } from 'react-router-dom';
 
 export default function SignUp(props){
+
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function submitForm() {
+        const payload = {
+            fullName,
+            email,
+            password
+        }
+
+        let url = 'users/sign';
+
+        await fetch(url, {
+            method: 'POST',
+            headers: new Headers({
+                'Content-Type':'application/json'
+            }),
+            body: JSON.stringify(payload)
+        })
+        .then(res => res.json())
+        .then(data => {
+            localStorage.setItem('token', data.token);
+            props.history.push('/about');
+        })
+    }
+
     return (
         <div className='container'>
             <input 
                 type='text'
                 placeholder='Full Name'
+                onChange={(e) => setFullName(e.target.value)}
             />
             <input 
                 type='email'
                 placeholder='Email Address'
+                onChange={(e) => setEmail(e.target.value)}
             />
             <input 
                 type='password'
                 placeholder='Password'
+                onChange={(e) => setPassword(e.target.value)}
             />
             <input
                 className='action-btn'
                 type='button'
                 value='Sign Up'
+                onClick={submitForm}
             />
             <Link 
-                className="SignIn"
+                className="second-action-btn"
                 to='/signin'
                 key='signin'
             >

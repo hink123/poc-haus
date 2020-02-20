e469fb5f4176511bc2f664362d2080e90fe4c1cb
devan <devanrterry@gmail.com>
2020-02-20 14:09:54 -0800

    set up explore deals page

---
+++ src/components/HomePageDeals.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePageDeals.css';
+import Deal from './Deal'
+
+
+export default function HomePageDeals(props){
+    return (
+        <div>
+            <h1>HomePageDeals</h1>
+            <Deal/>
+        </div>
+    )
+}
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './LandingPage.css';
-import img from '../images/down-arrow.png'
+import HomePageDeals from './HomePageDeals';
 
 export default function LandingPage(props){
     return (
@@ -8,13 +8,18 @@ export default function LandingPage(props){
         <div id='landing'>
             <h1>P.O.C.Haus</h1>
             <p>Explore, Shop, and Support POC-owned businesses in LA</p>
-            <img className='nav-arrow' src={img} />
+            <div className='nav-arrow'/>
         </div>
         <div id='about'>
             <h1>About</h1>
             <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <img className='nav-arrow' src={img} />
+            <div className='nav-arrow'/>
             {/* <p>How can I support?</p> */}
+        </div>
+        <div id="deals">
+            <h1>Explore Deals</h1>
+            <div className='nav-arrow'/>
+            <HomePageDeals/>
         </div>
         </div>
     )

5b4729bffe5a38d3373534858fbc596b03768b67
devan <devanrterry@gmail.com>
2020-02-19 21:04:02 -0800

    add product model

---
+++ models/product.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+
+const productSchema = new mongoose.Schema(
+  {
+    name: String,
+    image: String,
+    businessId: {
+        type: mongoose.Types.ObjectId,
+        ref: Business
+    },
+    URL: String,
+    description: String,
+    discount: String,
+    price: Number
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);

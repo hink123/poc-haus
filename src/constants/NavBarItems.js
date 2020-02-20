f3e23fd080b595e346952c9bb96ddf5403248f38
Daniel Shamany <dshamany@gmail.com>
2020-02-19 17:52:29 -0800

    split signin from signup

--- src/constants/NavBarItems.js
+++ src/constants/NavBarItems.js
@@ -11,7 +11,12 @@ export default {
         },
         signin: {
             title: "Sign In",
-            path: "/sign",
+            path: "/signin",
             key: "signin"
-        }  
+        },
+        signup: {
+            title: "Sign Up",
+            path: "/signup",
+            key: "signup"
+        } 
 };

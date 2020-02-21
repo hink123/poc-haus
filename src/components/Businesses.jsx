910e535cbb81f24ed3d3b55a30ecd4ed75ae6e1e
devan <devanrterry@gmail.com>
2020-02-20 18:30:20 -0800

    set up business component and resized

--- src/components/Businesses.jsx
+++ src/components/Businesses.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Businesses.css';
+import Business from './Business'
 
 export default function Businesses(props){
     return (
         <div>
             <h1>Businesses</h1>
+            <Business />
         </div>
     )
     }

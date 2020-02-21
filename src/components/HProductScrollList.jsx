8b1d2a8afd069f2a236b505469c7000023659152
Daniel Shamany <dshamany@gmail.com>
2020-02-21 14:31:57 -0800

    Add Abstract scroll component for products

--- src/components/HProductScrollList.jsx
+++ src/components/HProductScrollList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Product from './Product'
 
-export default function VerticalScrollList(props){
+export default function HProductScrollList(props){
     return (
         <div>
             {
